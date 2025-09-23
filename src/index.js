b83ce5b497930abc0d6916587725d86cfe700f93
Bryan <bryan.jonathan97@gmail.com>
2025-09-22 22:51:50 -0300

    implement store and logs

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
-render(
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+    <Provider store={store}>
+      <BrowserRouter>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
